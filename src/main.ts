bd17a6fce1c830afc84dbeb6696a2db436278b1c
Emmanuel Bastien <os@ebastien.name>
2022-09-18 17:21:36 +0200

    feat: minimal language client

---
+++ src/main.ts
@@ -0,0 +1,51 @@
+
+import {
+    ExtensionContext,
+    WorkspaceConfiguration,
+    workspace
+} from 'vscode';
+
+import {
+    Executable,
+    LanguageClient,
+    LanguageClientOptions,
+    ServerOptions
+} from 'vscode-languageclient/node';
+
+let client: LanguageClient;
+
+export function activate(_context: ExtensionContext) {
+
+    const config: WorkspaceConfiguration = workspace.getConfiguration("oal-server");
+
+    const serverPath = config.get<null | string>("serverPath") ?? "oal-server";
+
+    const serverExec: Executable = {
+        command: serverPath
+    }
+
+    const serverOptions: ServerOptions = {
+        run: serverExec,
+        debug: serverExec
+    };
+
+    const clientOptions: LanguageClientOptions = {
+        documentSelector: [{ scheme: 'file', language: 'oal' }]
+    };
+
+    client = new LanguageClient(
+        'oal-server',
+        'OpenAPI Language Server',
+        serverOptions,
+        clientOptions
+    );
+
+    client.start();
+}
+
+export function deactivate(): Thenable<void> | undefined {
+    if (!client) {
+        return undefined;
+    }
+    return client.stop();
+}
